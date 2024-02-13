cf892ebaa1a522dec5d8bf835aee770ae7ea317c
kanginsung <yk832@naver.com>
2024-02-13 16:56:23 +0900

    init

---
+++ src/main/java/com/ideate/ideaapiserver/IdeaApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.ideate.ideaapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideate/ideaapiserver/IdeaApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideate.ideaapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
