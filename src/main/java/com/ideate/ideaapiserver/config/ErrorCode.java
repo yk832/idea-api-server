a6577ab467484628cd13b24ef9c338e2cc59c461
kanginsung <yk832@naver.com>
2024-02-14 22:43:17 +0900

    이미지 에러코드 추가

--- src/main/java/com/ideate/ideaapiserver/config/ErrorCode.java
+++ src/main/java/com/ideate/ideaapiserver/config/ErrorCode.java
@@ -11,7 +11,7 @@ public enum ErrorCode {
     NOT_FOUND(404,"PAGE NOT FOUND"),
     NOT_FOUND_MEMBER(404,"존재하지 않는 고객"),
     INTERNAL_SERVER_ERROR(500,"서버에 장애가 발생하였습니다.\n관리자에게 문의해 주세요."),
-    ;
+    NOT_FOUND_RESOURCE(404, "존재하지 않는 이미지 리소스");
 
     private final Integer status;
 
