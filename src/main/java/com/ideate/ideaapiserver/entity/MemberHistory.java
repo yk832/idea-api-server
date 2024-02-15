7c7a2e995c1766d5fd19f02c093c67a6abc2729c
kanginsung <yk832@naver.com>
2024-02-15 12:18:05 +0900

    #3 member history entity 수정
    Resoruce 엔티티 연관관계 제거

--- src/main/java/com/ideate/ideaapiserver/entity/MemberHistory.java
+++ src/main/java/com/ideate/ideaapiserver/entity/MemberHistory.java
@@ -3,19 +3,23 @@ package com.ideate.ideaapiserver.entity;
 import com.ideate.ideaapiserver.config.HistoryType;
 import com.ideate.ideaapiserver.config.MemberEntityListener;
 import com.ideate.ideaapiserver.dto.member.MemberDto;
+import com.ideate.ideaapiserver.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.util.StringUtils;
 
+import java.util.Objects;
+import java.util.Optional;
+
 @Entity
-@Table(name = "MEMBER_HISTORY")
+@Table(name = "MEMBER_HISTORYS")
 @Getter
 @ToString
 @AllArgsConstructor
 @Builder(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode(of = "id")
-public class MemberHistory {
+@EqualsAndHashCode(of = "id", callSuper = true)
+public class MemberHistory extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,13 +37,18 @@ public class MemberHistory {
 
     private String mdn;
 
-//    @OneToOne(fetch = FetchType.LAZY)
-//    private Resource resource;
+    // TODO image 관련 필드 @Embedded 변경하기 (Resource entity)
+    private String imgPath;
+
+    private String fakeImgName;
+
+    private String originalImgName;
 
     @Enumerated(EnumType.STRING)
     private HistoryType historyType;
 
     public static MemberHistory create(Member member, HistoryType historyType) {
+        Resource resource = member.getResource();
         return MemberHistory.builder()
                 .uid(member.getUid())
                 .password(member.getPassword())
@@ -47,7 +56,9 @@ public class MemberHistory {
                 .nickname(member.getNickname())
                 .birthday(member.getBirthday())
                 .mdn(member.getMdn())
-//                .resource(member.getResource())
+                .imgPath(Objects.nonNull(resource) ? resource.getPath() : null)
+                .fakeImgName(Objects.nonNull(resource) ? resource.getFakeName() : null)
+                .originalImgName(Objects.nonNull(resource) ? resource.getOriginalName() : null)
                 .historyType(historyType)
             .build();
     }
