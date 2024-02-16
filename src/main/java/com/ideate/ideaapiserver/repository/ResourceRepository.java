3b79f9e6e62a9d7384d69f8f70c1530b13138315
kanginsung <yk832@naver.com>
2024-02-16 14:12:25 +0900

    리소스 entity fetch join 추가

--- src/main/java/com/ideate/ideaapiserver/repository/ResourceRepository.java
+++ src/main/java/com/ideate/ideaapiserver/repository/ResourceRepository.java
@@ -1,10 +1,21 @@
 package com.ideate.ideaapiserver.repository;
 
 import com.ideate.ideaapiserver.entity.Resource;
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"member"})
+    Optional<Resource> findById(Long id);
+    
+    @Override
+    @EntityGraph(attributePaths = {"member"})
+    List<Resource> findAll();
 
 }
