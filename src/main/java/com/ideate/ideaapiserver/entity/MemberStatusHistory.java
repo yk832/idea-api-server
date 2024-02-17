7f17bdf64ebeae77eee83e69baec8d2b1209731a
kanginsung <yk832@naver.com>
2024-02-18 01:05:14 +0900

    #5 create member-status-history document (mongoDB)

---
+++ src/main/java/com/ideate/ideaapiserver/entity/MemberStatusHistory.java
@@ -0,0 +1,58 @@
+package com.ideate.ideaapiserver.entity;
+
+import com.ideate.ideaapiserver.config.constant.MemberStatus;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Builder
+@Document(collection = "member_status_histories")
+public class MemberStatusHistory {
+
+    @Id
+    private String id;
+
+    private String uid;
+
+    private Long loginCount;
+
+    private MemberStatus memberStatus;
+
+    private LocalDateTime firstLoginDate;
+
+    private LocalDateTime lastLoginDate;
+
+
+    public static MemberStatusHistory create(Member member) {
+        return MemberStatusHistory.builder()
+                .uid(member.getUid())
+                .loginCount(1L)
+                .memberStatus(member.getMemberStatus())
+                .firstLoginDate(LocalDateTime.now())
+                .lastLoginDate(LocalDateTime.now())
+            .build();
+    }
+
+    public static MemberStatusHistory create(Member member, Long loginCount) {
+        return MemberStatusHistory.builder()
+                .uid(member.getUid())
+                .loginCount(loginCount)
+                .memberStatus(member.getMemberStatus())
+                .lastLoginDate(LocalDateTime.now())
+            .build();
+    }
+
+    public void updateStatus(MemberStatus memberStatus) {
+        this.memberStatus = memberStatus;
+    }
+
+    public void incrementLoginCount(Long loginCount) {
+        this.loginCount = loginCount;
+    }
+}
